7ceee1b94cfc14daaca65247c137d96014aa623b
Cássio Cunha <cassio@cassio.eti.br>
2020-05-07 19:16:21 -0300

    Upload New Files

--- capitulo12/instance.tf
+++ capitulo12/instance.tf
@@ -1,7 +1,8 @@
 resource "google_compute_instance" "vm0" {
-  name         = "instancia0"
-  machine_type = "n1-standard-1"
-  zone         = "southamerica-east1-a"
+
+	name = format ("%s-%s", terraform.workspace,"instancia0")
+	machine_type = "n1-standard-1"
+	zone = "southamerica-east1-a"
 
   boot_disk {
     initialize_params {
@@ -14,7 +15,7 @@ resource "google_compute_instance" "vm0" {
   }
 
   network_interface {
-    network = default 
+    network = "default"
 
     access_config {
     }
